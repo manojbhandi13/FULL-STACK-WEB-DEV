f1cabb8163ffe1465b32bd1031c49022952c6b16
manojbhandi13 <alphaleadsalpha@gmail.com>
2026-06-13 19:53:29 +0530

    Solved PracticeSet 1 & understood string speciality is JS

---
+++ WebClassroom/JAVASCRIPT/p1_q1.js
@@ -0,0 +1,21 @@
+const product = {
+    productName : "Parker Jotter Standard CT Ball Pen (Black)",
+    costPrice : 285,
+     discount : "5%",
+     sellingPrice : 270,
+     reviewNum : 7002,
+    rating : 4,
+    isDeal : true,
+    status : "Deal of the Day"
+};
+
+console.log(product);
+
+let s = "123";
+console.log(s + 1);
+
+let s1 = "abcd";
+console.log(s1 + 123);
+
+let s2 = "123";
+console.log(s2 + 123);
---
+++ WebClassroom/JAVASCRIPT/p1_q2.js
@@ -0,0 +1,20 @@
+const profile = {
+    userName : "Manoj Bhandi",
+    userId : "@manojbhandi",
+    postsNum : 4,
+    followersNum : 569,
+    followingNum : 438,
+    threadsId : "@manojbhandi",
+    isFollow : true
+};
+
+console.log(profile);
+
+console.log(typeof profile);
+console.log(typeof profile["userName"]);
+console.log(typeof profile["userId"]);
+console.log(typeof profile["postsNum"]);
+console.log(typeof profile["followersNum"]);
+console.log(typeof profile["followingNum"]);
+console.log(typeof profile["threadsId"]);
+console.log(typeof profile["isFollow"]);
