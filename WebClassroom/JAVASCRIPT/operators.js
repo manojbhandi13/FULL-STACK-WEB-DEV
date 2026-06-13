15808fb18e8cab768d2d05dab5abb0b7ef63e04f
manojbhandi13 <alphaleadsalpha@gmail.com>
2026-06-13 22:47:04 +0530

    Operators in JS; Special case in Comparison operators

---
+++ WebClassroom/JAVASCRIPT/operators.js
@@ -0,0 +1,56 @@
+let a = 5;
+let b = 2;
+// let c = a / b;
+console.log("a + b = ", a + b); //Addition
+console.log("a - b = ", a - b); //Subtraction
+console.log("a * b = ", a * b); //Multiplication
+console.log("a / b = ", a / b); //Division
+console.log("a =", a, "& b =", b);
+console.log("a % b =", a % b); //Modulus (Remainder)
+console.log("a ** b =", a ** b); //Exponentiation
+console.log("b ** a =", b ** a);
+
+//Unary Operators
+console.log("Unary Operators: ");
+
+//Post Increment Operator
+console.log("b++ =", b++); //b++ = 2
+console.log("b =", b); //b = 3
+
+//Post Decrement Operator
+console.log("a-- =", a--); //a-- = 5
+console.log("a =", a); //a = 4 
+
+//Pre Increment Operator
+console.log("++b =", ++b); //++b = 4
+
+//Pre Decrement Operator
+console.log("--a =", --a); //--a = 3
+
+//Comparison Operators
+console.log("Comparison Operators:");
+
+let x = 5; //number
+let y = "5"; //string -> number
+
+//Equal To
+console.log("x == y is", x == y); //true
+
+//Equal To & Type
+console.log("x === y is", x === y); //false (strict; data type comparison also takes place)
+
+//Not Equal To
+console.log("x != y is", x != y); //false
+
+//Not Equal To & Type
+console.log("x !== y is", x !== y); //true
+
+// <
+// >
+// <=
+// >=
+
+//Logical Operators
+// &&
+// ||
+// !
