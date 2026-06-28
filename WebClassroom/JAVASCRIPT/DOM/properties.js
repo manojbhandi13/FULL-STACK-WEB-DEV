aa0cd4a662bbeb552ef8d05cfa0f69da150a18bc
manojbhandi13 <alphaleadsalpha@gmail.com>
2026-06-28 15:19:46 +0530

    learn: DOM element properties - tagName, innerText, innerHTML, textContent

---
+++ WebClassroom/JAVASCRIPT/DOM/properties.js
@@ -0,0 +1,24 @@
+let tag = document.querySelector("li");
+
+//tagName
+console.dir(tag);
+console.log(tag.tagName);
+console.dir(tag.tagName);
+
+//innerText
+let div = document.querySelector("div");
+console.dir(div);
+console.dir(div.innerText);
+console.log(div.innerText);
+console.dir(tag.innerText);
+
+//innerHTML
+console.dir(div.innerHTML);
+console.dir(tag.innerHTML);
+
+//textContent
+heading = document.querySelector("h1");
+console.dir(heading);
+console.dir(heading.innerText);
+console.dir(heading.textContent);
+console.dir(heading.innerHTML);
